93b22da301ce905edf7f18089febf88a473d946a
KuuShyn <magalonakaj@proton.me>
2026-03-14 01:45:11 +0800

    rewrote highlight logic

--- Scripts/plantable.gd
+++ Scripts/plantable.gd
@@ -1,16 +1,19 @@
+# plantable.gd
 extends StaticBody2D
 
 @export var sprout_plant: PackedScene
-@onready var sprite: Sprite2D = $Sprite2D	
+@onready var sprite: Sprite2D = $Sprite2D
 
 func plant():
 	var sprout = sprout_plant.instantiate()
 	sprout.position = position
 	get_parent().add_child(sprout)
+	Gamestate.add_points(10)
 	queue_free()
 
-func set_highlight(active:bool) -> void:
-	if active:
-		sprite.modulate = Color("775946ff")
-	else:
-		sprite.modulate = Color("dfa988")
+func _on_actionable_area_entered(area: Area2D) -> void:
+	sprite.modulate = Color("775946ff")
+
+
+func _on_actionable_area_exited(area: Area2D) -> void:
+	sprite.modulate = Color("dfa988")
