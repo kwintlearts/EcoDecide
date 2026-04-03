c5f7bcd7903186b79b59d5a0b5dec177fbe53ea5
KuuShyn <49732964+KuuShyn@users.noreply.github.com>
2026-04-03 13:07:21 +0800

    removed lots of assets and added lots of assets

--- Scripts/sapling.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-
-func _ready() -> void:
-	add_to_group("sapling")  # ← Add to group for easy detection!
-	animated_sprite.set_frame_and_progress(0, 0.0)
